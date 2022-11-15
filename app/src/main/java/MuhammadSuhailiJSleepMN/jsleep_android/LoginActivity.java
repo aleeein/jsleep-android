46c2c9207adcd1adba8e22bf32018614f3fb5fc6
Muhammad Suhaili <suhailialeeee@gmail.com>
2022-11-15 17:58:33 +0700

    Case Study Modul 8

---
+++ app/src/main/java/MuhammadSuhailiJSleepMN/jsleep_android/LoginActivity.java
@@ -0,0 +1,34 @@
+package MuhammadSuhailiJSleepMN.jsleep_android;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView register = findViewById(R.id.RegisterNow);
+        TextView mainActivity = findViewById(R.id.loginButton);
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+        mainActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent move = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(move);
+            }
+        });
+    }
+}
