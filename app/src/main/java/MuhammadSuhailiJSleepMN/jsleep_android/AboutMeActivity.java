d396eaa609109e1589ce4d0734646524bcb560d5
Muhammad Suhaili <suhailialeeee@gmail.com>
2022-11-23 22:37:32 +0700

    Penyesuaian pada AboutMeActivity

--- app/src/main/java/MuhammadSuhailiJSleepMN/jsleep_android/AboutMeActivity.java
+++ app/src/main/java/MuhammadSuhailiJSleepMN/jsleep_android/AboutMeActivity.java
@@ -1,14 +1,23 @@
 package MuhammadSuhailiJSleepMN.jsleep_android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
+import MuhammadSuhailiJSleepMN.jsleep_android.model.Account;
 
 public class AboutMeActivity extends AppCompatActivity {
+    TextView name, email, balance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+        name = findViewById(R.id.answerName);
+        email = findViewById(R.id.answerEmail);
+        balance = findViewById(R.id.answerBalance);
+        name.setText(MainActivity.loginToMain.name);
+        email.setText(MainActivity.loginToMain.email);
+        String balanceAboutMe = String.valueOf(MainActivity.loginToMain.balance);
+        balance.setText(balanceAboutMe);
     }
+
 }
